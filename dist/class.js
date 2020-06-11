8b8f33e05beeabf73518d79b7a9f5cf70c8c93fa
Yuichiro Komori <thekomori1113@gmail.com>
2020-06-12 02:48:23 +0900

    learned class extends

--- dist/class.js
+++ dist/class.js
@@ -1,9 +1,24 @@
 'use strict'
 var Person = /** @class */ (function () {
-  function Person(initName) {
-    this.name = initName
+  function Person(name, age) {
+    this.name = name
+    this.age = age
+  }
+  Person.prototype.incrementAge = function () {
+    this.age += 1
+  }
+  // greeting(this: { name: string }) {
+  Person.prototype.greeting = function () {
+    console.log(
+      'Hello! My name is ' + this.name + '. I am ' + this.age + ' years old.',
+    )
   }
   return Person
 })()
-var quill = new Person('Quill')
-console.log(quill)
+var quill = new Person('Quill', 21)
+quill.greeting()
+// const anotherQuill = {
+//   name: 'anotherQuill',
+//   greeting: quill.greeting,
+// }
+// anotherQuill.greeting()
--- src/class.ts
+++ src/class.ts
@@ -1,24 +1,37 @@
 class Person {
-  name: string
+  constructor(public readonly name: string, protected age: number) {}
 
-  constructor(initName: string) {
-    this.name = initName
+  incrementAge() {
+    this.age += 1
   }
 
   // greeting(this: { name: string }) {
   greeting(this: Person) {
-    console.log(`Hello! My name is ${this.name}`)
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`)
   }
   // allow functionはthisが定義時に決まるので必要無い
   // greetingArrow = () => {
   //   console.log(`Hello! My name is ${this.name}`)
   // }
 }
-const quill = new Person('Quill')
+const quill = new Person('Quill', 21)
 quill.greeting()
 
-const anotherQuill = {
-  name: 'anotherQuill',
-  greeting: quill.greeting,
+// const anotherQuill = {
+//   name: 'anotherQuill',
+//   greeting: quill.greeting,
+// }
+// anotherQuill.greeting()
+
+class Teacher extends Person {
+  constructor(name: string, age: number, public subject: string) {
+    super(name, age)
+  }
+  greeting() {
+    console.log(
+      `Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}.`,
+    )
+  }
 }
-anotherQuill.greeting()
+const teacher = new Teacher('Quill', 38, 'Math')
+teacher.greeting()
